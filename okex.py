a123a727f927e0b2f7653b0e3c49844c149607fb
bboxhe <bboxhe@gmail.com>
2017-11-30 20:49:35 +0800

    Finish data  acquisition.

--- okex.py
+++ okex.py
@@ -1,15 +1,20 @@
-#!python
+#!/usr/bin/env python3
 
 import sys
 import signal
+import zlib
 import websocket
 import json
 import time
 
 class OKEX:
     def freshTicker(self, deJson):
+        _channel = deJson[0]['channel']
         data = deJson[0]['data']
-        print(data)
+        _buy = data['buy']
+        _sell = data['sell']
+        print('channel:%s\tbuy:%-10s\tsell:%-10s' % (_channel, _buy, _sell))
+        #print(data)
         # _high = data['high']
         # _last = data['last']
         # _low = data['low']
@@ -19,14 +24,23 @@ class OKEX:
         # print('ETH:high=%-7s low=%-7s last=\033[;;34m%-7s\033[0m open=%-7s close=%-7s'
                # % (_high, _low, _last, _open, _close))
 
+    def freshDepth(self, deJson):
+        _channel = deJson[0]['channel']
+        data = deJson[0]['data']
+        print(data)
+
     def on_message(self, ws, msg):
-        #print(msg)
-        deJson = json.loads(msg)
-        channel = deJson[0]['channel']
+        decompress = zlib.decompressobj(
+                -zlib.MAX_WBITS  # see above
+        )
+        inflated = decompress.decompress(msg)
+        inflated += decompress.flush()
+        deJson = json.loads(inflated.decode('utf-8'))
+        channel = deJson[0]['channel'];
         if channel == 'ok_sub_spot_eth_usdt_ticker':
             self.freshTicker(deJson)
-        else:
-            print(msg)
+        elif channel == 'ok_sub_spot_eth_usdt_depth_5':
+            self.freshDepth(deJson)
 
     def on_error(self, ws, error):
         print(error)
@@ -35,9 +49,10 @@ class OKEX:
         print("#### closed ###")
 
     def on_open(self, ws):
-        ws.send("{'event':'addChannel','channel':'ok_sub_spot_eth_usdt_ticker'}")
-        # ws.send("{'event':'addChannel','channel':'ok_sub_spot_eth_btc_ticker'}")
-        # ws.send("{'event':'addChannel','channel':'ok_sub_spot_btc_usdt_ticker'}")
+        ws.send("{'event':'addChannel','channel':'ok_sub_spot_eth_usdt_ticker','binary':'1'}")
+        ws.send("{'event':'addChannel','channel':'ok_sub_spot_eth_usdt_depth_5','binary':'1'}")
+        #ws.send("{'event':'addChannel','channel':'ok_sub_spot_eth_btc_ticker'}")
+        #ws.send("{'event':'addChannel','channel':'ok_sub_spot_btc_usdt_ticker'}")
         print('### opening ###')
 
     def connect(self):
@@ -57,7 +72,5 @@ def sigint_handler(signum,frame):
 
 if __name__ == "__main__":
     signal.signal(signal.SIGINT, sigint_handler)
-    print('### start ###')
-    print('### start ###')
     okex = OKEX()
     okex.connect()
